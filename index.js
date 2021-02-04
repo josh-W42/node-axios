2f4d33b410d3f270afa9c76005949010966806d6
jwilson42 <wilson.jd022@gmail.com>
2021-02-04 11:42:27 -0800

    axios successfully setup and now making omdb api calls on route

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 // This gives us access to a .env file in folder. We need this to access API keys.
 // It's important because you don't want your API key to be used
 
+const axios = require('axios');
+// This is what we're using in order to make requests
+// as opposed to the node-fetch module.
+
 // Old stuff
 const express = require('express');
 const app = express();
@@ -23,6 +27,25 @@ app.use(methodOveride('_method'));
 // ROUTES
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+// A new route that performs an api request
+app.get('/omdb', (req, res) => {
+    const query = {
+        params: {
+            s: 'star wars',
+            apiKey: process.env.API_KEY
+        }
+    }
+
+    axios.get('http://www.omdbapi.com', query)
+    .then(response => {
+        const data = response.data;
+        res.render('search.ejs', { data });
+    }).catch(error => {
+        console.log(`An Error has occured, status: ${error.response.status} - ${error.response.statusText}`);
+        res.send('An error has occured');
+    });
 });
 
 // LISTENER
